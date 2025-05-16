7dac74e0fc12111fb96968cb06bcfdfd73c8c90f
giuseppebrandi-byui <bra22041@byui.edu>
2025-05-16 07:53:41 +0100

    Priority Queue tests completed

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,62 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Test that the item (which contains both data and priority) is added to the back of the queue.
+    // Expected Result: [John (Pri:3), Bonny (Pri:5)]
+    // Defect(s) Found: None
+    public void TestPriorityQueueAddItem()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("John", 3);
+        priorityQueue.Enqueue("Bonny", 5);
+        Assert.AreEqual(priorityQueue.ToString(), "[John (Pri:3), Bonny (Pri:5)]");
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: Test that the Dequeue function removes the item with the highest priority.
+    // Expected Result: Francesco
+    // Defect(s) Found: Change line 27 in PriorityQueue from index < _queue.Count - 1 to index < _queue.Count;
+    public void TestPriorityQueueHighestPriority()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("John", 3);
+        priorityQueue.Enqueue("Bonny", 4);
+        priorityQueue.Enqueue("Jane", 1);
+        priorityQueue.Enqueue("Francesco", 5);
+        priorityQueue.Enqueue("Mark", 2);
+        var expected = priorityQueue.Dequeue();
+        Assert.AreEqual(expected, "Francesco");
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: If there are more than one item with the highest priority, then the item closest to the front of the queue will be removed and its value returned.
+    // Expected Result: Francesco
+    // Defect(s) Found: Change line 29 in PriorityQueue from if (_queue[index].Priority >= _queue to if (_queue[index].Priority > _queue.
+    public void TestPriorityQueueFirstHighestPriority()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("John", 1);
+        priorityQueue.Enqueue("Francesco", 4);
+        priorityQueue.Enqueue("Jane", 2);
+        priorityQueue.Enqueue("Mark", 3);
+        priorityQueue.Enqueue("Bonny", 4);
+        var expected = priorityQueue.Dequeue();
+        Assert.AreEqual(expected, "Francesco");
+    }
+    [TestMethod]
+    // Scenario: If the queue is empty, then an error exception shall be thrown. 
+    // Expected Result: The queue is empty.
+    // Defect(s) Found: Nothing found
+    public void TestPriorityQueueCheckIfEmpty()
+    {
+        var priorityQueue = new PriorityQueue();
+        try
+        {
+            priorityQueue.Dequeue();
+        }
+        catch (InvalidOperationException err)
+        {
+            Assert.AreEqual("The queue is empty.", err.Message);
+        }
+    }
 }
