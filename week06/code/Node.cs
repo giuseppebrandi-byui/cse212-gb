7083fa92706d076181c0222a23bd61a9c4d9303d
giuseppebrandi-byui <bra22041@byui.edu>
2025-06-11 07:16:55 +0100

    Resolved 4 challenges

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -21,7 +21,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
@@ -34,12 +34,47 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            if (Left is not null)
+            {
+                return Left.Contains(value);
+            }
+        }
+        else if (value > Data)
+        {
+            if (Right is not null)
+            {
+                return Right.Contains(value);
+            }
+        }
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (Left == null && Right == null)
+        {
+            return 1;
+        }
+
+        var heightRight = 0;
+        var heightLeft = 0;
+
+        if (Left is not null)
+        {
+            heightLeft = Left.GetHeight();
+        }
+        if (Right is not null)
+        {
+            heightRight = Right.GetHeight();
+        }
+
+        return (Math.Max(heightLeft, heightRight) + 1); // Replace this line with the correct return statement(s)
     }
 }
